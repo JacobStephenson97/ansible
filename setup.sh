fbe573f5a97dd200cf8c182dbb0a71f4aac34b00
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-03-27 12:45:41 -0400

    Added noconfirm flag

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 passwd
-pacman-key --init
+pacman-key --init 
 pacman-key --populate
-pacman -Syu
-pacman -S archlinux-keyring
-pacman -S sudo
+pacman -Syu --noconfirm
+pacman -S archlinux-keyring --noconfirm
+pacman -S sudo --noconfirm
 
 echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
 useradd -m -G wheel -s /bin/bash jacob
 passwd jacob
 su jacob
 
-pacman -S ansible
+#pacman -S ansible --noconfirm
 
 
