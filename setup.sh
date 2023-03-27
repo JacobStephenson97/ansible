17bd0f7528b43ce52ac0be54446f5da67659abe4
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-03-27 12:58:45 -0400

    change to just curl as root

--- setup.sh
+++ setup.sh
@@ -5,15 +5,10 @@ pacman-key --populate
 pacman -Syu --noconfirm
 pacman -S archlinux-keyring --noconfirm
 pacman -S sudo --noconfirm
+pacman -S ansible --noconfirm
 
 echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
 useradd -m -G wheel -s /bin/bash jacob
 passwd jacob
-su jacob
-cd ~
 
-sudo pacman -S ansible --noconfirm
-
-curl -s https://raw.githubusercontent.com/JacobStephenson97/ansible/main/setup.yml >> setup.yml
-
-ansible-playbook setup.yml
+curl -s https://raw.githubusercontent.com/JacobStephenson97/ansible/main/setup.yml >> /home/jacob/setup.yml
