fdc4d61f7712ff48ef439af45e83eeda30dd700f
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-03-27 12:42:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@ pacman-key --populate
 pacman -Syu
 pacman -S archlinux-keyring
 pacman -S sudo
+
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
+useradd -m -G wheel -s /bin/bash jacob
+passwd jacob
+su jacob
+
 pacman -S ansible
 
 
