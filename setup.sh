2312df1043ec42b929a4727c45a84eb29b3d8b4b
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-03-28 11:01:02 -0400

    rollback

--- setup.sh
+++ setup.sh
@@ -12,6 +12,6 @@ useradd -m -G wheel -s /bin/bash jacob
 passwd jacob
 
 su -c "git clone https://github.com/JacobStephenson97/ansible ~/setup" jacob
-su -c "mv ~/setup/.ssh ~/.ssh" jacob
+su -c "mv ~/setup/.ssh/*.pub ~/.ssh" jacob
 
 su -c "ansible-playbook ~/setup/local.yml --ask-vault-pass --ask-become-pass" jacob
