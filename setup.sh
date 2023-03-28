a3bd603695011ce691d5a5e74680d30861c1be31
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-03-28 12:16:47 -0400

    update script to install yay

--- setup.sh
+++ setup.sh
@@ -11,6 +11,13 @@ echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
 useradd -m -G wheel -s /bin/bash jacob
 passwd jacob
 
+su -c "git clone https://aur.archlinux.org/yay.git ~/yay" jacob
+su -c "cd ~/yay && makepkg -si && rm -rf ~yay" jacob
+
+
 su -c "git clone https://github.com/JacobStephenson97/ansible ~/setup" jacob
 
 su -c "ansible-playbook ~/setup/local.yml --ask-vault-pass --ask-become-pass" jacob
+
+
+
