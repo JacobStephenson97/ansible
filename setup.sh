4e54928553d454c1d9ac64d2c790bb1482d6ba65
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-03-28 13:05:10 -0400

    try this

--- setup.sh
+++ setup.sh
@@ -11,12 +11,6 @@ echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
 useradd -m -G wheel -s /bin/bash jacob
 passwd jacob
 
-su -c "git clone https://aur.archlinux.org/yay.git ~/yay" jacob
-export BUILDDIR=/home/jacob/yay/build
-export PKGDEST=/home/jacob/yay/packages
-su -c "cd ~/yay && makepkg -si --noconfirm --needed && rm -rf ~yay" jacob
-
-
 su -c "git clone https://github.com/JacobStephenson97/ansible ~/setup" jacob
 
 su -c "ansible-playbook ~/setup/local.yml --ask-vault-pass --ask-become-pass" jacob
